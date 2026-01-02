6dbcfd043a790b800fdf3be65cd7dc75a6b72fac
Aditya Upadhayay <adityaupadhayay066@gmail.com>
2026-01-02 12:52:07 +0530

    Swapping the number using thrid variable

---
+++ C++/Swapping.cpp
@@ -0,0 +1,21 @@
+# include<iostream>
+using  namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a : ";
+    cin>>a;
+    cout<<"Enter the value of b : ";
+    cin>>b;
+   cout<<"Before swapping : "<<endl;
+   cout<<"Value of a is : "<<a<<endl;   
+    cout<<"Value of b is : "<<b<<endl;
+    int c;
+  c=a;
+  a=b;
+  b=c;
+  cout<<"After swaping :"<<endl;
+   cout<<"Value of a is : "<<a<<endl;   
+    cout<<"Value of b is : "<<b<<endl;
+  
+
+}
