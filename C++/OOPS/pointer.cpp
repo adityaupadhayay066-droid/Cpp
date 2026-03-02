91ede4dfc91b6db70a372001d81c1d8d2d373fe9
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-03-02 17:39:52 +0530

    Remove old C++ files

--- C++/OOPS/pointer.cpp
+++
@@ -1,9 +0,0 @@
-# include<iostream>
-using namespace std;
-int main (){
-    int a = 20;
-    int*p=&a; // * is dereferencing operator 
-    *p=50;
-    cout<<"Value of a is: "<<a<<endl;
-    cout<<"Value of p is : "<<*p<<endl;
-}
