0862c91e6d4becf52b102baf40e18e4a8b490072
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-05-29 09:49:31 +0530

    Add alphabetic triangle pattern

---
+++ C++/Patterns/Ch_Trainglepattern.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n here: ";
+    cin>>n;
+    char ch='A';
+    for(int i=1; i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<ch<<" ";
+        }
+        ch++;
+        cout<<endl;
+    }
+}
