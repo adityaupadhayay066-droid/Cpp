31077468028e31ddbd64676b0fd822c1a33850e9
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-05-31 05:22:32 +0530

    Character type Floyd's traingle pattern program

---
+++ C++/Patterns/Char_Floyd's_Triangle.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter value of n here: ";
+cin>>n;
+char ch='A';
+for(int i=0;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<ch<<" ";
+        ch++;
+
+    }
+    cout<<endl;
+}
+}
