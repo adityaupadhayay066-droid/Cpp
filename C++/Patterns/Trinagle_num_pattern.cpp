5d9b447747baaa27bc696365c26a3bdf870024b3
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-06-09 11:00:00 +0530

    Add number pattern printing program

---
+++ C++/Patterns/Trinagle_num_pattern.cpp
@@ -0,0 +1,13 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n here: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=0 ;j<=i;j++){
+           cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
