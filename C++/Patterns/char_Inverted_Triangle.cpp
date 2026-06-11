77b5959534a0472dd149afb757313b2e74da4640
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-06-11 14:28:36 +0530

    Adding inverted triangle program in c++ using char

---
+++ C++/Patterns/char_Inverted_Triangle.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n here: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        char ch='A'+i;
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<n-1;j++){
+            cout<<ch;
+        }
+        cout<<endl;
+
+    }
+}
